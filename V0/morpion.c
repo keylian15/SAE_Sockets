e7b4497f3001db32fd7b97b2e0447aa243cbb301
garniik <antoine.arnoultfr@gmail.com>
2025-01-07 09:31:58 +0100

    place

--- V0/morpion.c
+++ V0/morpion.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "morpion.h"
 
+
+
 bool isFull(Morpion *m)
 {
     for (int i = 0; i < 3; i++)
@@ -9,4 +11,31 @@ bool isFull(Morpion *m)
         /* code */
     }
     
+}
+
+
+void initialise(){
+
+}
+void show(){
+
+}
+bool isValid(cell){
+    if (cell>3 || cell<3)
+    {
+       return false;
+    }
+    else{
+        return true;
+    }
+}
+/**
+ * place dans la grille la forme demannder
+ * place(&m,0,2,x)
+ * @param cellx coordone x de la cellule
+ * @param celly coordone y de la cellule
+ * @param form la forme qui est entrée
+ */
+void place(Morpion *m,int cellx,int celly,char form){
+    return m->grille[cellx][celly]=form;
 }
