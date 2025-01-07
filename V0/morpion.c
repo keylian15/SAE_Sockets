99393575069e2014905902e0b946f1e9d0b8cafb
Keylian Turbé <keylian.turbe@univ-littoral.fr>
2025-01-07 17:48:26 +0100

    Version FInal Terminé

--- V0/morpion.c
+++ V0/morpion.c
@@ -1,6 +1,13 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "morpion.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #define PORT 5000
 #define LG_MESSAGE 256
 
@@ -48,8 +55,8 @@ void show(const Morpion *m)
         if (i < 2)
             printf("-----\n");
     }
+    printf("\n\n");
 }
-
 
 /**
  * verifie si la cellule existe
@@ -69,7 +76,6 @@ bool isValid(int cell)
     }
 }
 
-
 /**
  * place dans la grille la forme demannder
  * place(&m,3,x);
@@ -83,7 +89,7 @@ void place(Morpion *m, int cell, char form)
     {
         for (int celly = 0; celly < 3; celly++)
         {
-            if (count==cell)
+            if (count == cell)
             {
                 m->grille[cellx][celly] = form;
                 return;
@@ -98,134 +104,155 @@ void jeuClient(int descripteurSocket)
     // Déclaration des variables.
     char messageEnvoye[LG_MESSAGE];
     char messageRecu[LG_MESSAGE];
-    int nb;
+    int bytesReceived;
+    int bytesSent;
     bool condition = false;
 
     Morpion jeu;
     initialise(&jeu);
 
+    printf("Début du jeu.\n");
     while (1)
     {
         // Montre la grille
         show(&jeu);
         int choix;
-
+        condition = false;
         while (!condition)
         {
             // Demander au client de choisir une case
-            printf("Choisissez votre case (1 à 9) : ");
+            printf("Choisissez votre case (1 à 9) :\n ");
             scanf("%d", &choix);
 
             // Envoi le choix au serveur.
             snprintf(messageEnvoye, sizeof(messageEnvoye), "%d", choix);
-            send(descripteurSocket, messageEnvoye, strlen(messageEnvoye) + 1, 0);
+            bytesSent = send(descripteurSocket, messageEnvoye, strlen(messageEnvoye) + 1, 0);
+            verifEnvoye(bytesSent, messageEnvoye);
 
             // ====== Réception Message Serveur ======
             memset(messageRecu, 0x00, LG_MESSAGE);
-            nb = recv(descripteurSocket, messageRecu, LG_MESSAGE, 0);
-            if (nb <= 0)
-            {
-                perror("Erreur lors de la réception...");
-                close(descripteurSocket);
-                continue;
-            }
-            else if (strcmp(messageRecu, "confirm") == 0)
+            bytesReceived = recv(descripteurSocket, messageRecu, LG_MESSAGE, 0);
+            verifRecu(bytesReceived, messageRecu);
+
+            if (strcmp(messageRecu, "confirm") == 0)
             {
                 condition = true;
             }
         }
-        printf("Sorti de boucle\n");
 
         // Placer la case du client coté client.
         place(&jeu, choix, 'X');
         show(&jeu);
 
         // Recevoir la mise à jour
-        nb = recv(descripteurSocket, messageRecu, LG_MESSAGE, 0);
-        if (nb <= 0)
-        {
-            perror("Erreur lors de la réception...");
-            close(descripteurSocket);
-            exit(-5);
-        }
+        bytesReceived = recv(descripteurSocket, messageRecu, LG_MESSAGE, 0);
+        verifRecu(bytesReceived, messageRecu);
 
         // Place la case du serveur coté client.
+        printf("Au serveur de jouer : \n");
         int case_serveur = atoi(messageRecu);
         place(&jeu, case_serveur, 'O');
-
-        show(&jeu);
     }
 }
 
-
 void jeuServeur(int socketDialogue)
 {
-    printf("Début du jeu\n");
+    printf("Début du jeu.\n");
     // Déclaration des variables.
-    int nb;
+    int bytesSent;
+    int bytesReceived;
     char messageRecu[LG_MESSAGE];
+    char messageEnvoye[LG_MESSAGE];
 
     Morpion jeu;
     initialise(&jeu);
 
     // ====== Envoi Message de départ ======
-    char message[LG_MESSAGE] = "start";
-    send(socketDialogue, message, strlen(message) + 1, 0);
-
+    strcpy(messageEnvoye, "start");
+    bytesSent = send(socketDialogue, messageEnvoye, strlen(messageEnvoye) + 1, 0);
+    verifEnvoye(bytesSent, messageEnvoye);
     // ====== Boucle de jeu ======
     while (1)
     {
         // ====== Réception Message Client ======
         memset(messageRecu, 0x00, LG_MESSAGE);
-        nb = recv(socketDialogue, messageRecu, LG_MESSAGE, 0);
-        if (nb <= 0)
-        {
-            perror("Erreur lors de la réception...");
-            close(socketDialogue);
-            continue;
-        }
+        bytesReceived = recv(socketDialogue, messageRecu, LG_MESSAGE, 0);
+        verifRecu(bytesReceived, messageRecu);
+
         // Conversion de la case choisie en int
         int case_client = atoi(messageRecu);
 
         // Verification choix.
-        printf("Case client joué %d\n", case_client);
-
         if (!isValid(case_client))
         {
-            char erreur[LG_MESSAGE] = "erreur";
-            send(socketDialogue, erreur, strlen(erreur) + 1, 0);
-            printf("Incorect.\n");
+            strcpy(messageEnvoye, "erreur");
+            bytesSent = send(socketDialogue, messageEnvoye, strlen(messageEnvoye) + 1, 0);
+            verifEnvoye(bytesSent, messageEnvoye);
         }
         else
         {
-            printf("Corect.\n");
 
-            char confirm[LG_MESSAGE] = "confirm";
-            send(socketDialogue, confirm, strlen(confirm) + 1, 0);
+            strcpy(messageEnvoye, "confirm");
+            bytesSent = send(socketDialogue, messageEnvoye, strlen(messageEnvoye) + 1, 0);
+            verifEnvoye(bytesSent, messageEnvoye);
 
-            printf("avant showwww\n");
             // Placer la case du client coté serveur.
             place(&jeu, case_client, 'X');
             show(&jeu);
-            printf("apres showwww\n");
+
             // Le serveur joue en choisissant une case au hasard
             int case_serveur = rand() % 9 + 1;
-            place(&jeu, case_serveur, 'O');
 
             // Envoie de la case serveur.
-            char case_serveur_str[LG_MESSAGE];
-            snprintf(case_serveur_str, LG_MESSAGE, "%d", case_serveur);
-            send(socketDialogue, case_serveur_str, strlen(case_serveur_str) + 1, 0);
-
-            // POUR LA V1
-            // while (jeu.grille[(case_serveur - 1) / 3][(case_serveur - 1) % 3] != ' ')
-            // {
-            //     case_serveur = rand() % 9 + 1;
-            // }
+            messageEnvoye[LG_MESSAGE];
+            snprintf(messageEnvoye, LG_MESSAGE, "%d", case_serveur);
+            bytesSent = send(socketDialogue, messageEnvoye, strlen(messageEnvoye) + 1, 0);
+            verifEnvoye(bytesSent, messageEnvoye);
+            
+            place(&jeu, case_serveur, 'O');
+            show(&jeu);
         }
     }
 
     // ====== Fermeture Socket Dialogue (8) ======
     close(socketDialogue);
     printf("Socket de dialogue fermée.\n");
+}
+
+void verifEnvoye(ssize_t bytesSent, const char *messageEnvoye)
+{
+    if (bytesSent < 0)
+    {
+        perror("Erreur lors de l'envoi du message");
+        // Gestion d'erreur ici (ex: fermer socket, quitter, etc.)
+    }
+    else if ((size_t)bytesSent != strlen(messageEnvoye) + 1)
+    {
+        fprintf(stderr, "Tous les octets n'ont pas été envoyés (%zd/%zd)\n",
+                bytesSent, strlen(messageEnvoye) + 1);
+        // Gestion pour les envois partiels (ré-envoi possible ici)
+    }
+    else
+    {
+        printf("Message envoyé '%s' avec succès (%zd octets)\n", messageEnvoye, bytesSent);
+    }
+}
+
+void verifRecu(ssize_t bytesReceived, char *messageRecu)
+{
+    if (bytesReceived < 0)
+    {
+        perror("Erreur lors de la réception du message");
+        // Gestion d'erreur ici (ex: fermer socket, quitter, etc.)
+    }
+    else if (bytesReceived == 0)
+    {
+        printf("Le socket a été fermé par l'émetteur.\n");
+        // Fin de connexion propre, gestion ici si nécessaire
+    }
+    else
+    {
+        messageRecu[bytesReceived] = '\0'; // Terminer correctement la chaîne reçue
+        printf("Message reçu '%s' avec succès (%zd octets) \n", messageRecu ,bytesReceived);
+    }
 }
--- V0/morpion.h
+++ V0/morpion.h
@@ -13,4 +13,10 @@ void initialise(Morpion *m);
 void show(const Morpion *m);
 bool isValid(int cell);
 void place(Morpion *m, int cell, char form);
+
+void jeuClient(int descripteurSocket);
+void jeuServeur(int socketDialogue);
+void verifEnvoye(ssize_t bytesSent, const char *messageEnvoye);
+void verifRecu(ssize_t bytesReceived, char *messageRecu);
+
 #endif
