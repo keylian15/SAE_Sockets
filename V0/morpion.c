93530de01f5d570504455e4ecae941f02470b505
Keylian Turbé <keylian.turbe@univ-littoral.fr>
2025-01-07 09:29:26 +0100

    Fini isFull

--- V0/morpion.c
+++ V0/morpion.c
@@ -10,12 +10,11 @@
  */
 bool isFull(Morpion *m)
 {
-
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            if (m->grille[i][j] == " ")
+            if (m->grille[i][j] == ' ') 
             {
                 return false;
             }
