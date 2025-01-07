4de1bd3e28c11ec493c30530fb9e8fffc5051156
garniik <antoine.arnoultfr@gmail.com>
2025-01-07 13:44:11 +0100

    checkwinner

--- V1/morpion.c
+++ V1/morpion.c
@@ -70,6 +70,28 @@ void show(const Morpion *m)
             printf("-----\n");
     }
 }
+    /**
+     * Vérifie si la grille est entièrement remplie.
+     * isFull(&m);
+     * @param m Pointeur vers la structure Morpion.
+     * @return true si la grille est pleine, false sinon.
+     */
+
+bool isFull(const Morpion *m)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (m->grille[i][j] == ' ')
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 
 /**
  * verifie si la cellule existe
@@ -102,7 +124,7 @@ void place(Morpion *m, int cell, char form)
     {
         for (int celly = 0; celly < 3; celly++)
         {
-            if (count==cell)
+            if (count == cell)
             {
                 m->grille[cellx][celly] = form;
                 return;
@@ -112,10 +134,53 @@ void place(Morpion *m, int cell, char form)
 }
 
 /**
- * 
- * 
- * 
+ * verifie qui a gagner a l'aide d'une combinaison de coordonée
+ * whoWin(&m,1,1); *
+ * @return char
  */
-void checkWin(Morpion *m){
-    
+char whoWin(Morpion *m, int x, int y)
+{
+
+    if (m->grille[x][y] == "x")
+    {
+        return "le gagnant est le client";
+    }
+    else
+    {
+        return "le gagnant est le serveur";
+    }
+}
+/**
+ * verifie si il y a un gagnant dans la grille et renvoie le gagnant si oui
+ * checkwin(&m);
+ * @return char
+ */
+char checkWin(Morpion *m)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (m->grille[i][1] == m->grille[i][0] && m->grille[i][2] == m->grille[i][0])
+        {
+            return whoWin(&m, i, 1);
+        }
+    }
+    for (int j = 0; j < 3; j++)
+    {
+        if (m->grille[0][j] == m->grille[1][j] && m->grille[2][j] == m->grille[0][j])
+        {
+            return whoWin(&m, 1, j);
+        }
+    }
+    if (m->grille[0][0] == m->grille[1][1] && m->grille[2][2] == m->grille[1][1])
+    {
+        return whoWin(&m, 1, 1);
+    }
+    else if (m->grille[0][3] == m->grille[1][1] && m->grille[2][0] == m->grille[1][1])
+    {
+        return whoWin(&m, 1, 1);
+    }
+    else
+    {
+        return "pas de gagnant on continue";
+    }
 }
