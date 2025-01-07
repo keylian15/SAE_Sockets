1362ba72006e1f0781421b88594740ad38665601
Keylian Turbé <keylian.turbe@univ-littoral.fr>
2025-01-07 09:28:28 +0100

    Fini isFull

--- V0/morpion.c
+++ V0/morpion.c
@@ -2,11 +2,24 @@
 #include <stdio.h>
 #include "morpion.h"
 
+/**
+ * Vérifie si la grille est entièrement remplie.
+ * isFull(&m);
+ * @param m Pointeur vers la structure Morpion.
+ * @return true si la grille est pleine, false sinon.
+ */
 bool isFull(Morpion *m)
 {
+
     for (int i = 0; i < 3; i++)
     {
-        /* code */
+        for (int j = 0; j < 3; j++)
+        {
+            if (m->grille[i][j] == " ")
+            {
+                return false;
+            }
+        }
     }
-    
+    return true;
 }
